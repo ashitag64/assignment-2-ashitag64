8c20b3f62ad62ff550cd69bd528d7c87fb93bb0f
ashitag64 <ashita.goyal_cs18@gla.ac.in>
2020-03-28 03:37:16 +0530

    add getter methods for fields in node class

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -8,8 +8,24 @@ package problem1.node;
 
 public class TreeNode {
 
+    //fields for node class
     private int data;
     private TreeNode leftNode;
     private  TreeNode rightNode;
 
+    //getter methods for the fields
+    public int getData() {
+        return data;
+    }
+
+    public TreeNode getLeftNode() {
+        return leftNode;
+    }
+
+    public TreeNode getRightNode() {
+        return rightNode;
+    }
+
+
 }
+
