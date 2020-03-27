1d35012e22dc34ec41c5464cd97e3faa8ca330a1
ashitag64 <ashita.goyal_cs18@gla.ac.in>
2020-03-28 04:26:25 +0530

    print the desired result

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -8,7 +8,6 @@ package problem2.main;
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 import problem1.mybst.MyBinarySearchTree;
-
 import java.util.Scanner;
 
 public class MyMain {
@@ -23,8 +22,10 @@ public class MyMain {
         }
         System.out.println("Pre - Order Traversal: ");
         myBinarySearchTree.preOrder();
+        System.out.println();
         System.out.println("Post - Order Traversal: ");
         myBinarySearchTree.postOrder();
+        System.out.println();
         System.out.println("Condition A: Root element is placed at opposite end, i.e. in pre order root is the first element where as in post order root is the last element is TRUE.");
         System.out.println("Condition B: They have common mid point, i.e. both the traversal will give same element at the mid position for odd number of nodes is FALSE.");
     }
