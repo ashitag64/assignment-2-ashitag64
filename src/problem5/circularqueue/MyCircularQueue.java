21321a650434aa57a294342195156633d70b159a
ashitag64 <ashita.goyal_cs18@gla.ac.in>
2020-03-28 19:09:35 +0530

    add fields to the MyCircularQueue class

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -5,6 +5,16 @@
  *  Time: 7:06 PM
  */
 package problem5.circularqueue;
+
+import problem5.node.Node;
+
 //to implement circular queue
 public class MyCircularQueue {
+
+    //add fields to the MyCircularQueue class
+    Node front;
+    Node rear;
+    int size;
+
+
 }
