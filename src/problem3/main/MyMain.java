28763b66c53d97f174b4baf984ac5863c098f1ee
ashitag64 <ashita.goyal_cs18@gla.ac.in>
2020-03-28 13:50:19 +0530

    complete main class of problem3

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -7,8 +7,22 @@
 package problem3.main;
 // executable class
 // use problem5.student.Student class to create object of student
+import problem3.myqueue.MyPriorityQueue;
 import problem5.student.Student;
 
-public class MyMain {
+import java.util.Scanner;
 
+public class MyMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int rollNo =  scanner.nextInt();
+        String name = scanner.nextLine();
+        int backLogCounter = scanner.nextInt();
+        int appearingCounter = scanner.nextInt();
+        Student student = new Student(rollNo, name, backLogCounter, appearingCounter);
+        MyPriorityQueue myPriorityQueue = new MyPriorityQueue();
+        myPriorityQueue.insert(student);
+        System.out.println("Details of student as per the roll number,in ascending order: ");
+        myPriorityQueue.print();
+    }
 }
